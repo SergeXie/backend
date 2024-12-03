4afab4ec2575eeb9a06654e82228f616bf732be3
Hua <13030119817@163.com>
2024-12-03 16:53:26 +0800

    卓始策略15 更新

--- backend/utils/strategys/zuoshi15_strategy_20240823110347_9760.py
+++ backend/utils/strategys/zuoshi15_strategy_20240823110347_9760.py
@@ -2,12 +2,13 @@ import pytz
 import backtrader as bt
 from utils.public_strategy import CommonStrategy
 
+
 class ZuoShi15Strategy(CommonStrategy):
     params = (
-        ('price_diff', 15),  # 当日价格波动阈值，当日单边走落差15美元
-        ('trade_time', 17),  # 下单开始时间点：北京时间17点
+        ('price_diff', 15),  # 当日单边走落差15美元
+        ('trade_time', 16),  # 下单开始时间点：UTC时间16:00，对应北京时间22:00
         ('risk_reward_ratio', 1.0),  # 止盈/止损比
-        ('lot_size', 1),  # 每次交易的手数
+        ('lot_size', 0.1),  # 每次交易的手数
     )
 
     def __init__(self, indicator_params, goodsId=None, begin_time=None):
@@ -37,13 +38,15 @@ class ZuoShi15Strategy(CommonStrategy):
             if any(x.status in [bt.Order.Submitted, bt.Order.Accepted] for x in self.order):
                 return
 
-        # 获取当前北京时间
+        # print("时间：{}".format(self.datas[0].datetime.datetime(0).strftime('%Y-%m-%d %H:%M:%S')))
+
         utc_datetime = bt.num2date(self.data.datetime[0])
         local_datetime = utc_datetime
         local_hour = local_datetime.hour  # 获取北京时间的小时部分
 
         # 切换到新的一天时重置变量
         current_date = local_datetime.date()
+
         # 通过比较当前日期和self.cur_day，判断是否进入新的一天。如果是新的一天，重置当天的最高价、最低价和相应的时间
         if self.cur_day != current_date:
             self.cur_day = current_date
@@ -55,9 +58,10 @@ class ZuoShi15Strategy(CommonStrategy):
 
             print(f"新的一天开始: {current_date}")
 
-        # 仅在北京时间17点之后进行交易
+        # 仅在北京时间22点之后进行交易
         if local_hour < self.params.trade_time:
-            return  # 如果当前时间未达到17:00，直接退出，不进行交易
+            print("local_hour:{}".format(local_hour))
+            return  # 如果当前时间未达到22:00，直接退出，不进行交易
 
         # 计算当天的最高价和最低价:
         if self.cur_high is None or self.data.high[0] > self.cur_high:
@@ -74,6 +78,11 @@ class ZuoShi15Strategy(CommonStrategy):
         # 检查反向K线并执行交易
         last_candle_bullish = self.data.close[-1] > self.data.open[-1]
         last_candle_bearish = self.data.close[-1] < self.data.open[-1]
+
+        print(f"当前日期：{current_date}, 当前时间：{local_hour}, 当前价格：{self.data.close[0]}")
+        print(f"当天最高价：{self.cur_high}, 最低价：{self.cur_low}, 价格波动差：{self.cur_high - self.cur_low}")
+        print(
+            f"上一根K线是 {'多头' if last_candle_bullish else '空头'}，当前K线是 {'多头' if self.data.close[0] > self.data.open[0] else '空头'}")
 
         if self.data.close[0] > self.data.open[0] and last_candle_bearish:
             # 如果前一根K线是阴线（空头），而当前K线是阳线（多头）， 执行买入操作（做多）
