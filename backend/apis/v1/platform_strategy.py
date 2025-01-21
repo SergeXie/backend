a4a2ccc1c8e7377b6c6a8fbf7338788f975015da
Hua <13030119817@163.com>
2025-01-21 16:43:54 +0800

    获取回测时间范围内浮动盈亏错误bug

--- backend/apis/v1/platform_strategy.py
+++ backend/apis/v1/platform_strategy.py
@@ -621,106 +621,115 @@ async def fetch_tester_result(request: Request):
 @router.post("/fetchFloatingProfit", name="获取回测时间范围内浮动盈亏")
 async def fetch_floating_profit(request: Request):
 
-    data_request = await request.json()
+    try:
+        data_request = await request.json()
 
-    testerUids = data_request.get("testerUids", [])  # 策略UID
+        testerUids = data_request.get("testerUids", [])  # 策略UID
 
-    async with async_db_session() as db:
-        # 根据 testerUids 查询历史回测结果 取得回测时间
-        query = await db.execute(select(
-            DqlStrategyTestResult).where(
-            DqlStrategyTestResult.uid.in_(testerUids),
-            DqlStrategyTestResult.is_delete == 0,
-            DqlStrategyTestResult.calculationStatus == 1))
+        async with async_db_session() as db:
+            # 根据 testerUids 查询历史回测结果 取得回测时间
+            query = await db.execute(select(
+                DqlStrategyTestResult).where(
+                DqlStrategyTestResult.uid.in_(testerUids),
+                DqlStrategyTestResult.is_delete == 0,
+                DqlStrategyTestResult.calculationStatus == 1))
 
-        dql_strategy_test_result_all = query.scalars().all()
+            dql_strategy_test_result_all = query.scalars().all()
 
-        result_list = []  # 存储最终的浮动盈亏数据
+            result_list = []  # 存储最终的浮动盈亏数据
 
-        for data in dql_strategy_test_result_all:
-            trader_result = json.loads(data.traderResult)
+            for data in dql_strategy_test_result_all:
+                trader_result = json.loads(data.traderResult)
 
-            starting_cash = trader_result.get("traderReport", {}).get("startingCash", 10000)  # 起始资金
+                starting_cash = trader_result.get("traderReport", {}).get("startingCash", 10000)  # 起始资金
 
-            # 订单记录 trader_orders
-            trader_orders = trader_result.get("traderResult", [])
+                # 订单记录 trader_orders
+                trader_orders = trader_result.get("traderResult", [])
 
-            # 拿起始时间-结束时间获取K线
-            start_time = data.startTime.strftime('%Y-%m-%d %H:%M:%S')
-            end_time = data.endTime.strftime('%Y-%m-%d %H:%M:%S')
-            select_model_class, goods_ = await select_goods_common(db, data.goodsId, model_classes)
-            if not select_model_class:
-                return await response_base.fail(msg="数据库表未找到！", data=[])
-            db_select_kline = select(select_model_class).where(
-                select_model_class.tradingGoods == goods_.trading_goods,
-                select_model_class.platform == goods_.platform,
-                select_model_class.type == data.period,
-                select_model_class.tradeDateTime.between(start_time, end_time))
+                # 拿起始时间-结束时间获取K线
+                start_time = data.startTime.strftime('%Y-%m-%d %H:%M:%S')
+                end_time = data.endTime.strftime('%Y-%m-%d %H:%M:%S')
+                select_model_class, goods_ = await select_goods_common(db, data.goodsId, model_classes)
+                if not select_model_class:
+                    return await response_base.fail(msg="数据库表未找到！", data=[])
+                db_select_kline = select(select_model_class).where(
+                    select_model_class.tradingGoods == goods_.trading_goods,
+                    select_model_class.platform == goods_.platform,
+                    select_model_class.type == data.period,
+                    select_model_class.tradeDateTime.between(start_time, end_time))
 
-            kline_datas = await select_kline_data(db, db_select_kline)
+                kline_datas = await select_kline_data(db, db_select_kline)
 
-            # === 初始化变量 ===
-            floating_pnl_list = []  # 存储浮动盈亏数据
+                # === 初始化变量 ===
+                floating_pnl_list = []  # 存储浮动盈亏数据
 
-            # 查找closeTime等于null的（持仓单子）
-            new_trader_orders = [x for x in trader_orders if x.get("closeTime", None)]
-            # 使用集合跟踪已出现的 tradeid
-            seen_tradeids = set()
-            # 生成新列表，仅包含 closeTime 为空的记录，且 tradeid 不重复
-            unique_open_trades = []
-            for trade_id in new_trader_orders:
-                seen_tradeids.add(trade_id["tradeid"])
+                # 查找closeTime等于null的（持仓单子）
+                new_trader_orders = [x for x in trader_orders if x.get("closeTime", None)]
+                # 使用集合跟踪已出现的 tradeid
+                seen_tradeids = set()
+                # 生成新列表，仅包含 closeTime 为空的记录，且 tradeid 不重复
+                unique_open_trades = []
+                for trade_id in new_trader_orders:
+                    seen_tradeids.add(trade_id["tradeid"])
 
-            for trade in trader_orders:
-                if trade.get("closeTime", None):
-                    unique_open_trades.append(trade)
-                elif not trade.get("closeTime", None) and trade.get("tradeid", None) not in seen_tradeids:
-                    unique_open_trades.append(trade)
+                for trade in trader_orders:
+                    if trade.get("closeTime", None):
+                        unique_open_trades.append(trade)
+                    elif not trade.get("closeTime", None) and trade.get("tradeid", None) not in seen_tradeids:
+                        unique_open_trades.append(trade)
 
-            starting_cash = starting_cash
-            # === 遍历 K 线计算浮动盈亏 ===
-            for kline in kline_datas:
-                current_price = kline["close"]  # K 线收盘价
-                current_date = kline["timestamp"]  # K 线时间
-                net_value = starting_cash  # 每根 K 线初始净值
-                profit = 0  # 初始化浮动盈亏
-                # 遍历所有交易订单，按时间顺序执行
-                for trade in unique_open_trades:
-                    order_type = trade["orderType"]
-                    close_time = trade.get("closeTime", None)
-                    open_time = trade.get("openTime", None)
-                    order_size = trade.get("size", None)
-                    open_price = trade.get("openPrice", None)
+                starting_cash = starting_cash
+                # === 遍历 K 线计算浮动盈亏 ===
+                for kline in kline_datas:
+                    current_price = kline["close"]  # K 线收盘价
+                    current_date = kline["timestamp"]  # K 线时间
+                    net_value = starting_cash  # 每根 K 线初始净值
+                    profit = 0  # 初始化浮动盈亏
+                    # 遍历所有交易订单，按时间顺序执行
+                    for trade in unique_open_trades:
+                        order_type = trade["orderType"]
+                        close_time = trade.get("closeTime", None)
+                        open_time = trade.get("openTime", None)
+                        if open_time:
+                            open_time = open_time
+                        else:
+                            open_time = trade.get("timestamp", None)
+                        order_size = trade.get("size", None)
+                        open_price = trade.get("openPrice", None)
 
-                    if close_time:
-                        # 计算浮动盈亏（持仓未平仓）
-                        if open_time < current_date and close_time > current_date:
+                        if close_time:
+                            # 计算浮动盈亏（持仓未平仓）
+                            if open_time < current_date and close_time > current_date:
+                                position = 1 if order_type == "buy" else -1
+                                net_value = net_value + (current_price - open_price) * position * order_size * goods_.profitRatio
+
+                        if open_time < current_date and not close_time:
                             position = 1 if order_type == "buy" else -1
                             net_value = net_value + (current_price - open_price) * position * order_size * goods_.profitRatio
 
-                    if open_time < current_date and not close_time:
-                        position = 1 if order_type == "buy" else -1
-                        net_value = net_value + (current_price - open_price) * position * order_size * goods_.profitRatio
+                        # 计算已实现盈亏（已平仓）
+                        if close_time and close_time <= current_date:
+                            net_value += trade["pnl"]
 
-                    # 计算已实现盈亏（已平仓）
-                    if close_time and close_time <= current_date:
-                        net_value += trade["pnl"]
+                    # 存储当前时间的浮动盈亏
+                    floating_pnl_list.append({
+                        "timestamp": current_date,
+                        "pnl": round(profit, 2),
+                        "netValue": round(net_value, 2)
+                    })
 
-                # 存储当前时间的浮动盈亏
-                floating_pnl_list.append({
-                    "timestamp": current_date,
-                    "pnl": round(profit, 2),
-                    "netValue": round(net_value, 2)
+                # === 返回最终计算结果 ===
+                result_list.append({
+                    "testerUid": data.uid,
+                    "floatingPnl": floating_pnl_list
                 })
 
-            # === 返回最终计算结果 ===
-            result_list.append({
-                "testerUid": data.uid,
-                "floatingPnl": floating_pnl_list
-            })
+            return await response_base.success(data=result_list)
 
-        return await response_base.success(data=result_list)
-
+    except Exception as e:
+        info = traceback.format_exc()
+        log.error(f"获取回测时间范围内浮动盈亏错误：{info}")
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
 
 @router.post("/syncBatchTest", name="策略批量回测(同步)")
 async def indicator_sync_batch_test(request: Request):
