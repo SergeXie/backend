46d2ec0a79b44757ec95fc57e3ff64ce13df24d1
Hua <13030119817@163.com>
2025-07-11 17:06:58 +0800

    新增指标

--- backend/utils/common.py
+++ backend/utils/common.py
@@ -32,6 +32,7 @@ from utils.indicators.bollinger import ResponseBollingerData
 from utils.indicators.ema import ResponseEMAData
 from utils.indicators.btatr import ResponseATRData
 from utils.indicators.rsi import ResponseRSIData
+from utils.indicators.wmPattern import ResponseWMData
 from pypinyin import lazy_pinyin, Style
 from typing import List, Dict, Any
 from utils.strategys import reload_strategies
@@ -75,6 +76,7 @@ indicator_classes = {
     "btATR": ResponseATRData,
     "RSI": ResponseRSIData,
     "SMCP": ResponseSMCPData,
+    "WM": ResponseWMData,
 }
 
 
