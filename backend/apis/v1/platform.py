e360269cf0df7031a76577a3b3d58812cee544ad
Hua <13030119817@163.com>
2025-08-04 17:45:26 +0800

    update

--- backend/apis/v1/platform.py
+++ backend/apis/v1/platform.py
@@ -67,11 +67,11 @@ async def select_kline_orders(goods: str, period: str, beginTime: str, endTime: 
                 and_(
                     DqlOrder.tradingGoods == goods,
                     DqlOrder.period == period,
-                    DqlOrder.timestamp.between(beginTime, endTime),
+                    DqlOrder.openTime.between(beginTime, endTime),
                     DqlOrder.strategyUid == strategyUid,
                 )
             )
-            .order_by(DqlOrder.timestamp.asc())
+            .order_by(DqlOrder.openTime.asc())
         )
         result = await db.execute(stmt)
         rows = result.scalars().all()
